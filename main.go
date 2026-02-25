157cffe26ecb756530f5e3f9a784a998c3842fee
DH <adlandh@gmail.com>
2026-02-25 22:35:34 +0100

    refactor(main): simplify sender initialization and remove else nesting

--- main.go
+++ main.go
@@ -24,14 +24,16 @@ func main() {
 	}
 
 	httpClient := &http.Client{Timeout: env.Timeout}
-	if sender, err := adapters.NewPushoverClient(env.Pushover, httpClient); err != nil {
-		log.Fatalf("error creating sender: %v\n", err)
-	} else {
-		useCase := application.NewSendNotificationUseCase(sender)
-		mcpServer := ports.NewServer(serverName, serverVersion, useCase)
 
-		if err := server.ServeStdio(mcpServer); err != nil {
-			log.Fatalf("error starting server: %v\n", err)
-		}
+	sender, err := adapters.NewPushoverClient(env.Pushover, httpClient)
+	if err != nil {
+		log.Fatalf("error creating sender: %v\n", err)
+	}
+
+	useCase := application.NewSendNotificationUseCase(sender)
+	mcpServer := ports.NewServer(serverName, serverVersion, useCase)
+
+	if err := server.ServeStdio(mcpServer); err != nil {
+		log.Fatalf("error starting server: %v\n", err)
 	}
 }
